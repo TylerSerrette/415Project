91af99a5a38c860d61edd067d8c434bc48fce5e2
TylerSerrette <76139849+TylerSerrette@users.noreply.github.com>
2023-04-05 18:08:04 -0500

    Create 415project.js

---
+++ 415project.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const bodyParser=require('body-parser');
+const app = express();
+const port = 3000;
+var fs = require("fs");
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes will go here
+
+app.get('/', function(req, res) {
+  const myquery = req.query;
+  var outstring = 'Starting... ';
+  res.send(outstring);
+});
+
+
+// Write to a file 
+
+app.get('/wfile', function(req, res) {
+  const myquery = req.query;
+  
+  var outstring = '';
+  for(var key in myquery) { outstring += "--" + key + ">" + myquery[key]; }
+  fs.appendFile("mydata.txt", outstring+'\n', (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("File written successfully\n");
+      console.log("Contents of file now:\n");
+      console.log(fs.readFileSync("mydata.txt", "utf8"));
+    }
+  });
+ 
+  res.send(outstring);
+
+});
+
+
+// Simple cascade
+app.param('name', function(req, res, next, name) {
+  const modified = name.toUpperCase();
+
+  req.name = modified;
+  next();
+});
+
+
+// A POST request
+
+app.post('/post/users', function(req, res) {
+  const user_id = req.body.id;
+  const token = req.body.token;
+  const geo = req.body.geo;
+
+  res.send({
+    'user_id': user_id,
+    'token': token,
+    'geo': geo
+  });
+});
+
+async function run() {
+  try {
+    const database = client.db('TMSdb');
+    const parts = database.collection('CMPS415');
+
+    // Hardwired Query for a part that has partID '12345'
+    // const query = { partID: '12345' };
+    // But we will use the parameter provided with the route
+    const query = { partID: req.params.item };
+
+    const part = await parts.findOne(query);
+    console.log(part);
+    res.send('Found this: ' + JSON.stringify(part));  //Use stringify to print a json
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+});
