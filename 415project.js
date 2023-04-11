2f74a70d9bc48a11da9b322648386dd00cf4badb
Tyler Serrette <76139849+TylerSerrette@users.noreply.github.com>
2023-04-10 21:32:17 -0500

    Update 415project.js

--- 415project.js
+++ 415project.js
@@ -69,16 +69,11 @@ app.get('/wfile', function(req, res) {
 
 // A POST request
 
-app.post('/post/users', function(req, res) {
-  const user_id = req.body.id;
-  const token = req.body.token;
-  const geo = req.body.geo;
-
-  res.send({
-    'user_id': user_id,
-    'token': token,
-    'geo': geo
-  });
+app.post('/rest/tickets', (req, res) => {
+  const ticket = req.body;
+  ticket.id = tickets.length + 1;
+  tickets.push(ticket);
+  res.json(ticket);
 });
 
 
